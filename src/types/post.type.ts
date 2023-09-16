bb8cb92f835057767f7393a137cb301cc5e6bf54
ilasorokin <revolut_mozart@icloud.com>
2023-09-16 22:41:53 +0300

    format

--- src/types/post.type.ts
+++ src/types/post.type.ts
@@ -1,7 +1,7 @@
 export type Post = {
-    id:string,
-    title:string,
-    body:string
-    createdAt:string,
-    updatedAt?:string
-}
+  id: string;
+  title: string;
+  body: string;
+  createdAt: string;
+  updatedAt?: string;
+};
